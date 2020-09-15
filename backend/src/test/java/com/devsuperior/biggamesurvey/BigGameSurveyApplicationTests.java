c914146730d9a5a194285975a6ba8046547c780a
David Chaves <davi.ch.fe@gmail.com>
2020-09-14 23:07:20 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/biggamesurvey/BigGameSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.biggamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigGameSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
